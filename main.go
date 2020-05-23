3830960ef66edadfa65ff766e1a1919033e4a1e0
leonnicolas <leonloechner@gmx.de>
2020-05-23 19:27:12 +0200

    main.go: cleaned up code

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -24,7 +25,8 @@ type Latency struct {
 
 type Vector struct {
 	Source    string    `json:"source"`
-	Latencies []Latency `json:"latencies"`
+	Latencies []Latency `json:"latencies,omitempty"`
+	Ok        bool      `json:"ok"`
 }
 
 type matrix []Vector
@@ -40,9 +42,15 @@ func (m matrix) String() string {
 	return s
 }
 
-func timeHTTPRequest(url string) *Latency {
+func timeHTTPRequest(ctx context.Context, url string) *Latency {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return &Latency{Destination: url}
+	}
 	start := time.Now()
-	_, err := http.Get(url)
+	if _, err := http.DefaultClient.Do(req); err != nil {
+		fmt.Printf("failed to make ping request: %v\n", err)
+	}
 	end := time.Now()
 	return &Latency{
 		Destination: url,
@@ -51,14 +59,14 @@ func timeHTTPRequest(url string) *Latency {
 	}
 }
 
-func getLatencies(urls []*url.URL) []*Latency {
+func getLatencies(ctx context.Context, urls []*url.URL) []*Latency {
 	var wg sync.WaitGroup
 	lats := make([]*Latency, len(urls))
 	for i := range urls {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			lats[i] = timeHTTPRequest(urls[i].String())
+			lats[i] = timeHTTPRequest(ctx, urls[i].String())
 		}(i)
 	}
 	wg.Wait()
@@ -89,7 +97,6 @@ func vectorHandler(defaultSRV string) func(http.ResponseWriter, *http.Request) {
 		var err error
 		if r.URL.Query()["srv"] != nil {
 			srv, err = srvFromRequest(r)
-			fmt.Println("using new srv in vector handler: " + srv)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
@@ -101,7 +108,7 @@ func vectorHandler(defaultSRV string) func(http.ResponseWriter, *http.Request) {
 			http.Error(w, err.Error(), http.StatusServiceUnavailable)
 			return
 		}
-		lats := getLatencies(urls)
+		lats := getLatencies(r.Context(), urls)
 		data, err := json.Marshal(lats)
 		if err != nil {
 			fmt.Println(err)
@@ -119,96 +126,92 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 func srvFromRequest(r *http.Request) (string, error) {
 	srv := r.URL.Query()["srv"][0]
 	if len(strings.SplitN(srv, ".", 3)) != 3 {
-		return "", errors.New("the given srv record name has no valid format. It should look something like _foo._tcp.foo.com\n")
+		return "", errors.New("the given SRV record name does not have a valid format; it should look something like _foo._tcp.example.com")
 	}
 	return srv, nil
 }
 
-func getLatenciesFrom(url *url.URL) ([]Latency, error) {
-	resp, err := http.Get(url.String())
+func getVectorFrom(ctx context.Context, url *url.URL) (*Vector, error) {
+	v := &Vector{
+		Source: url.String(),
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url.String(), nil)
 	if err != nil {
-		return nil, err
+		return v, err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return v, fmt.Errorf("failed to make GET request: %w", err)
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode == http.StatusServiceUnavailable {
-		return nil, errors.New("srv does not resolve\n")
+		return v, errors.New("failed to resolve SRV record")
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return v, fmt.Errorf("failed to read body: %w", err)
 	}
-	var lats []Latency
-	err = json.Unmarshal((body), &lats)
-	if err != nil {
-		return nil, errors.New("response from node has wrong format: Maybe it is not running this service?\n")
+	if err = json.Unmarshal((body), &v.Latencies); err != nil {
+		return v, fmt.Errorf("response from node has wrong format: maybe it is not running this service?: %w", err)
 	}
-	return lats, nil
+	v.Ok = true
+	return v, nil
 }
 
 func collectAllHandler(srv string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		srv_target := srv
+		target := srv
 		//srv target will be over written, if it is specified in the url query
 		if r.URL.Query()["srv"] != nil {
 			var err error
-			srv_target, err = srvFromRequest(r)
+			target, err = srvFromRequest(r)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
 		}
-		urls, err := resolveSRV(srv, "/vector", "srv="+srv_target)
+		urls, err := resolveSRV(srv, "/vector", "srv="+target)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 		//getting target urls: in case some nodes are down, we can still return a complete matrix with error entries
-		var tURLs []*url.URL
-		if srv_target != srv {
-			var err error
-			tURLs, err = resolveSRV(srv, "", "")
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			}
-		} else {
-			tURLs = urls
-		}
-		var lMatrix matrix
-		for _, u := range urls {
-			fmt.Println(u)
-			vec, err := getLatenciesFrom(u)
-			if err != nil {
-				for i := 0; i < len(tURLs); i++ {
-					vec = append(vec, Latency{
-						Destination: tURLs[i].String(),
-						Duration:    0,
-						Ok:          false,
-					})
+		m := make(matrix, len(urls))
+		var wg sync.WaitGroup
+		for i := range urls {
+			wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				vec, err := getVectorFrom(r.Context(), urls[i])
+				if err != nil {
+					fmt.Printf("failed to get Vector from %s: %v\n", vec.Source, err)
 				}
-			}
-			lVector := Vector{u.String(), vec}
-			lMatrix = append(lMatrix, lVector)
+				m[i] = *vec
+			}(i)
 		}
-		for _, lats := range lMatrix {
+		wg.Wait()
+		for _, lats := range m {
 			sort.Slice(lats.Latencies, func(i, j int) bool {
 				return lats.Latencies[i].Destination < lats.Latencies[j].Destination
 			})
 		}
-		sort.Slice(lMatrix, func(i, j int) bool {
-			return lMatrix[i].Source < lMatrix[j].Source
+		sort.Slice(m, func(i, j int) bool {
+			return m[i].Source < m[j].Source
 		})
 		if r.URL.Query()["json"] != nil && r.URL.Query()["json"][0] == "true" {
-
-			j, _ := json.Marshal(lMatrix)
+			j, err := json.Marshal(m)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
 			w.Write([]byte(j))
-		} else {
-			w.Write([]byte(lMatrix.String()))
+			return
 		}
+		w.Write([]byte(m.String()))
 	}
 }
 
-var srv *string = flag.String("srv", "_test._tcp.leon.squat.ai", "the srv record name to be used to look up IP addresses and port")
+var srv *string = flag.String("srv", "_service._proto.exmaple.com", "the srv record name to be used to look up IP addresses and port")
 var listenAddr *string = flag.String("listen-address", ":3000", "The service will be listening to that address with port\ne.g. 172.0.0.1:3000")
 
 func main() {
